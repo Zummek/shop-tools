968cd9c4c5b9732d7c2a1b69ddc87ac02e8a9400
Adam Jędryka <adam.jedryka98@gmail.com>
2024-06-12 11:04:00 +0200

    chore: fix deployed page displaying v9

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+
 import { GeneratePriceListPage } from './features/priceList/routes/GeneratePriceListPage/GeneratePriceListPage';
-import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      index: true,
+      element: <Navigate to="/generate-price-list" />,
+    },
+    {
+      path: '/generate-price-list',
+      index: true,
+      element: <GeneratePriceListPage />,
+    },
+  ],
   {
-    path: '/',
-    index: true,
-    element: <Navigate to="/generate-price-list" />,
-  },
-  {
-    path: '/generate-price-list',
-    index: true,
-    element: <GeneratePriceListPage />,
-  },
-]);
+    basename: '/shop-tools/',
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

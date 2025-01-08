b0f28964742972aa4d45966c19d96eab6b737084
Adam Jędryka <adam.jedryka98@gmail.com>
2025-01-08 18:07:06 +0100

    feat: Update NIP of the company

--- src/features/invoiceConverter/utils/detectInvoiceProvider.ts
+++ src/features/invoiceConverter/utils/detectInvoiceProvider.ts
@@ -9,7 +9,8 @@ export const detectInvoiceProvider = async (
   if (fileType === 'text/csv') return InvoideProvider.bezGluten;
   if (fileText.includes('544-112-43-37')) return InvoideProvider.dary;
   if (fileText.includes('9482624487')) return InvoideProvider.wiesiolek;
-  if (fileText.includes('5321690962')) return InvoideProvider.medicaline;
+  if (fileText.includes('5322116337') || fileText.includes('5321690962'))
+    return InvoideProvider.medicaline;
 
   return null;
 };
